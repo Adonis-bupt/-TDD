ab2734a2a1b9e228ac251cdbb1ec66546c6be331
Kyino_bupt <63436915+Adonis-bupt@users.noreply.github.com>
2022-06-07 22:21:42 +0800

    add a message summarising the above Chap7P31

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,29 @@ from django.test import TestCase
 from django.template.loader import render_to_string
 from lists.models import Item
 
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -24,55 +47,17 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 
 
-class HomePageTest(TestCase):
+class ListViewTest(TestCase):
 
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list/')
+        self.assertTemplateUsed(response, 'list.html')
 
-    def test_can_save_a_POST_request(self):
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirecets_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
+    def test_display_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list/')
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
-
-
-# class SmokeTest(TestCase):
-#     def test_root_url_resolves_to_home_page_view(self):
-#         found = resolve('/')
-#         self.assertEqual(found.func, home_page)
-#
-#     def test_home_page_returns_correct_html(self):
-#         # request = HttpRequest()
-#         # response = home_page(request)
-#         response=self.client.get('/')
-#
-#         html = response.content.decode('utf8')
-#
-#         # expected_html=render_to_string('home.html')
-#         # self.assertEqual(html,expected_html)
-#         self.assertTrue(html.startswith('<html'))
-#         self.assertIn('<title>To-Do lists</title>', html)
-#         self.assertTrue(html.endswith("</html>"))
-#
-#         self.assertTemplateUsed(response,'home.html')
-#
-#
