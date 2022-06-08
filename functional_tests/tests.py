df86af126ac3c33f1397242041fa576abd804a9b
Kyino_bupt <63436915+Adonis-bupt@users.noreply.github.com>
2022-06-09 01:42:35 +0800

    migrate to github
    generate git. Try to move it to server of Tencent Cloud

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,9 +11,12 @@ MAX_WAIT = 4
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://'+ staging_server
+        mode = 0
+        staging_server = '43.142.33.71'
+        # os.environ.get('STAGING_SERVER')
+        if mode == 1:
+            if staging_server:
+                self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
@@ -31,11 +34,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    # def test_can_a_list_for_one_user(self):
-    #     # Edith has heard about a cool new online to-do app.
-    #     self.browser.get(self.live_server_url)
-    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
-    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
     def test_multiple_users_can_start_list_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -104,45 +102,45 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10
         )
 
-    #
-    # def test_can_start_a_list_and_retrieve_it_later(self):
-    #     # Edith has heard about a cool new online to-do app. She goes
-    #     # to check out its homepage.
-    #     self.browser.get(self.live_server_url)
-    #
-    #     # She notices the page title and header mention to-do lists
-    #     self.assertIn('To-Do', self.browser.title)
-    #     header_text = self.browser.find_element_by_tag_name('h1').text
-    #     # header_text = self.browser.find_element(By.YAG_NAME,'h1').text
-    #     self.assertIn('To-Do', header_text)
-    #
-    #     # She is invited to enter a to-do item straight away
-    #
-    #     # She types "peacock feathers" into a text box (Edith's hobby
-    #     # is tying fly-fishing lures)
-    #
-    #     # When she hits enter, the page updates, and now the page lists
-    #     # "1: Buy peacock feathers: as an item in a to-do list"
-    #
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     inputbox.send_keys('Buy peacock feathers')
-    #     inputbox.send_keys(Keys.ENTER)
-    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
-    #     # There is still a text box inviting her to add another item. She
-    #     # enters "Use peacock feather s to make a fly" (Edith is very methodical)
-    #
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #
-    #     inputbox.send_keys('Use peacock feathers to make a fly')
-    #     inputbox.send_keys(Keys.ENTER)
-    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
-    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-    #     # The page updates again, and now shows both items on her list
-    #     # Edith wonders whether the site will remember her list, Then she sees
-    #     # that the site has generated a unique URL for her -- there is some
-    #     # explanatory text to that effect.
-    #     self.fail('Finish the test')
-    #
-    #     # She visits that URL - her to-do list is still there.
-    #
-    #     # Satisfied, she goes back to sleep
+    def test_can_a_list_for_one_user(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage.
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        # header_text = self.browser.find_element(By.YAG_NAME,'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers: as an item in a to-do list"
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feather s to make a fly" (Edith is very methodical)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # The page updates again, and now shows both items on her list
+        # Edith wonders whether the site will remember her list, Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        
+        # self.fail('Finish the test')
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
